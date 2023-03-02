85c5280311017f6b866245fbc118d2e8505da673
hgujong <jhseo0704@handong.ac.kr>
2023-03-02 22:58:35 +0900

    JAva Practice with Ban

---
+++ Main.java
@@ -0,0 +1,15 @@
+import lab1.Pencil;
+
+public class Main {
+
+    public static void main(String args[]){
+        int x;
+
+        Pencil p = new Pencil();
+
+        p.setLen(15);
+        x = p.getLen();
+
+        System.out.println(x);
+    }
+}
---
+++ lab1/Pencil.java
@@ -0,0 +1,14 @@
+package lab1;
+
+public class Pencil {
+    private int length;
+
+    public int setLen(int x) {
+        length = x;
+        return length;
+    }
+
+    public int getLen() {
+        return length;
+    }
+}
